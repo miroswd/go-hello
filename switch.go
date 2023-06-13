4b78e82a70da29c73a4bdbcb407247e723ff7389
miroswd <altamirsantos93@hotmail.com>
2023-06-12 22:34:21 -0300

    feat: switch case

---
+++ switch.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	x := 1
+
+	switch { // without expression, switch true {
+		case x < 5:
+			fmt.Println("Less than 5")
+		case x == 5:
+			fmt.Println("Equal to 5")
+		case x > 5:
+			fmt.Println("More than 5")
+	}
+
+
+	whoIAm := "miro"
+ 
+	switch whoIAm {
+		case "miro":
+			fmt.Println(`Hi miro`);
+		case "john":
+			fmt.Println("Hello john");
+		default:
+			fmt.Println("Hey there")
+	}
+
+
+	switch x {
+		case 1, 2, 3:
+			fmt.Println("Forbidden number")
+		case 4, 5:
+			fmt.Println("Ok")
+		default: 
+			fmt.Println("More than expected")
+	}
+
+
+}
