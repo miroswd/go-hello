77f0f9d0a50a49936da5fe1c264859e2f40061f6
miroswd <altamirsantos93@hotmail.com>
2023-06-22 23:25:39 -0300

    feat(channel): use comma ok to ensure the quit was closed

--- select-channel-oddeven.go
+++ select-channel-oddeven.go
@@ -17,7 +17,7 @@ func main() {
 
 
 func sendNumbers(oddChannel, evenChannel chan int, quit chan bool) {
-	total := 50
+	total :=10
 
 	for i := 0; i < total; i++ {
 		if i % 2 == 0 {
@@ -28,9 +28,21 @@ func sendNumbers(oddChannel, evenChannel chan int, quit chan bool) {
 	}
 
 	quit <- true
-
-	close(oddChannel)
+	
 	close(evenChannel)
+	close(oddChannel)
+	
+	/*
+	The number: 46 is even
+	The number: 47 is odd
+	The number: 48 is even
+	The number: 49 is odd
+	The number: 0 is even 
+	The number: 0 is odd
+
+	the zeros represent the closed channel
+	*/
+
 
 }
 
@@ -43,8 +55,12 @@ func receive (oddChannel, evenChannel chan int, quit chan bool) {
 			fmt.Println("The number:", v, "is odd")
 		case v := <- evenChannel:
 			fmt.Println("The number:", v, "is even")
-		case <- quit:
-			close(quit)
+		case v, ok := <- quit:
+			if !ok {
+				fmt.Println("Failed:", v)
+			} else {
+				fmt.Println("Finishing...", v)
+			}
 			return
 		}
 	}
